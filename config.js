e82804e674f5b488e5e5064b1b9886ca7d69c3b5
Hritik Kumar <hritik.kmr.111@gmail.com>
2021-11-28 16:50:27 +0530

    update config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-    MONGODB_URL : process.env.MONGODB_URL,
+    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost:27017/klimbdb',
     PORT : process.env.PORT || 5000
 }
